323420092d6f59586423020794d7f179f3a27002
Jan Macheta <jan.macheta@embe.tech>
2022-05-18 18:09:40 +0200

    added doxygen documentation

--- include/ecpp/endian.hpp
+++ include/ecpp/endian.hpp
@@ -12,55 +12,105 @@
 namespace ecpp {
 
     namespace impl {
-        constexpr std::uint8_t bswap(std::uint8_t v) noexcept {
-            return v;
+        /**
+         * @brief Returns n
+         *
+         * This function is implemented to make sure that every integral type may be handled by byteswap
+         * @param[in] n single byte unsigned integer
+         * @return n
+         */
+        constexpr std::uint8_t bswap(std::uint8_t n) noexcept {
+            return n;
         }
 
-        constexpr std::uint16_t bswap(std::uint16_t v) noexcept {
-            return static_cast<std::uint16_t>((v << 8U) | (v >> 8U));
+        /**
+         * @brief Reverses the bytes in the given integer value n
+         *
+         * @param[in] n two byte unsigned integer
+         * @return n with reversed byte order
+         */
+        constexpr std::uint16_t bswap(std::uint16_t n) noexcept {
+            return static_cast<std::uint16_t>((n << 8U) | (n >> 8U));
         }
 
-        constexpr std::uint32_t bswap(std::uint32_t v) noexcept {
-            return static_cast<std::uint32_t>(bswap(static_cast<std::uint16_t>(v))) << 16U | static_cast<std::uint32_t>(bswap(static_cast<std::uint16_t>(v >> 16U)));
+        /**
+         * @brief Reverses the bytes in the given integer value n
+         *
+         * @param[in] n four byte unsigned integer
+         * @return n with reversed byte order
+         */
+        constexpr std::uint32_t bswap(std::uint32_t n) noexcept {
+            return static_cast<std::uint32_t>(bswap(static_cast<std::uint16_t>(n))) << 16U | static_cast<std::uint32_t>(bswap(static_cast<std::uint16_t>(n >> 16U)));
         }
 
-        constexpr std::uint64_t bswap(std::uint64_t v) noexcept {
-            return static_cast<std::uint64_t>(bswap(static_cast<std::uint32_t>(v))) << 32U | static_cast<std::uint64_t>(bswap(static_cast<std::uint32_t>(v >> 32U)));
+        /**
+         * @brief Reverses the bytes in the given integer value n
+         *
+         * @param[in] n eight byte unsigned integer
+         * @return n with reversed byte order
+         */
+        constexpr std::uint64_t bswap(std::uint64_t n) noexcept {
+            return static_cast<std::uint64_t>(bswap(static_cast<std::uint32_t>(n))) << 32U | static_cast<std::uint64_t>(bswap(static_cast<std::uint32_t>(n >> 32U)));
         }
 
 
+        /// Concept that is true for every contiguous iterator that iterates over values with size of 1 byte
         template<typename T>
         concept ContiguousByteIt = std::contiguous_iterator<T> &&(1 == sizeof(typename std::iterator_traits<T>::value_type));
 
+        /// Concept that is true for every output and contiguous iterator that iterates over values with size of 1 byte
         template<typename T>
         concept ContiguousOutputByteIt = ContiguousByteIt<T> && std::output_iterator<T, typename std::iterator_traits<T>::value_type>;
     } // namespace impl
 
 
-    template<std::integral T> [[nodiscard]] constexpr T byteswap(T value) noexcept {
+    /**
+     * @brief Reverses the bytes in the given integer value n
+     *
+     * @tparam T type that sarisfies std::integral, i.e., T is an integer type. The program is ill-formed if T has padding bits
+     * @param[in] n integer value
+     * @note this function will use std::byteswap if possible
+     * @return An integer value of type T whose object representation comprises the bytes of that of n in reversed order
+     */
+    template<std::integral T> [[nodiscard]] constexpr T byteswap(T n) noexcept {
 #ifdef __cpp_lib_byteswap
-        return std::byteswap(value);
+        return std::byteswap(n);
 #else
-        if constexpr (1 == sizeof(value)) {
-            return static_cast<T>(impl::bswap(static_cast<std::uint8_t>(value)));
-        } else if constexpr (2 == sizeof(value)) {
-            return static_cast<T>(impl::bswap(static_cast<std::uint16_t>(value)));
-        } else if constexpr (4 == sizeof(value)) {
-            return static_cast<T>(impl::bswap(static_cast<std::uint32_t>(value)));
-        } else if constexpr (8 == sizeof(value)) {
-            return static_cast<T>(impl::bswap(static_cast<std::uint64_t>(value)));
+        if constexpr (1 == sizeof(n)) {
+            return static_cast<T>(impl::bswap(static_cast<std::uint8_t>(n)));
+        } else if constexpr (2 == sizeof(n)) {
+            return static_cast<T>(impl::bswap(static_cast<std::uint16_t>(n)));
+        } else if constexpr (4 == sizeof(n)) {
+            return static_cast<T>(impl::bswap(static_cast<std::uint32_t>(n)));
+        } else if constexpr (8 == sizeof(n)) {
+            return static_cast<T>(impl::bswap(static_cast<std::uint64_t>(n)));
         } else {
-            static_assert(0 == sizeof(value), "ecpp::byteswap is not implemented for type of this size");
+            static_assert(0 == sizeof(n), "ecpp::byteswap is not implemented for type of this size");
         }
 #endif
     }
 
 
-    template<std::integral T> [[nodiscard]] constexpr T ntoh(T value) noexcept {
-        return (std::endian::native == std::endian::big) ? value : byteswap(value);
+    /**
+     * @brief Convert value from network byte order to host byte order
+     *
+     * @tparam T type that sarisfies std::integral, i.e., T is an integer type. The program is ill-formed if T has padding bits
+     * @param[in] n integer value in network byte order
+     * @return An integer value of type T whose object representation comprises the bytes of that of n in host byte order
+     */
+    template<std::integral T> [[nodiscard]] constexpr T ntoh(T n) noexcept {
+        return (std::endian::native == std::endian::big) ? n : byteswap(n);
     }
 
-
+    /**
+     * @brief Returns value of type T by copying sizeof(T) bytes from src and converting from network byte order to host byte order
+     *
+     * @tparam T type that satisfies std::integral, i.e., T is an integer type. The program is ill-formed if T has padding bits
+     * @tparam CByteIt type that satisfies impl::ContiguousByteIt, i.e., CByteIt is contiguous iterator, that iterates over values with size of 1
+     * @param[in] src the beginning of the memory region in network byte order, which should be converted to T. src must point to memory region of at least sizeof(T) size, otherwise the behaviour is
+     * undefined
+     * @return An integer value of type T whose object representation comprises the bytes of src converted to host byte order
+     */
     template<std::integral T, impl::ContiguousByteIt CByteIt> [[nodiscard]] constexpr T ntoh(CByteIt src) noexcept {
         using value_type = std::remove_cvref_t<T>;
 
@@ -81,10 +131,19 @@ namespace ecpp {
     }
 
 
-    template<std::integral T, impl::ContiguousOutputByteIt COutByteIt> constexpr void ntoh(T value, COutByteIt dst) noexcept {
+    /**
+     * @brief Converts n from network byte order to host byte order, and copies it into dst
+     *
+     * @tparam T type that satisfies std::integral, i.e., T is an integer type. The program is ill-formed if T has padding bits
+     * @tparam COutByteIt type that satisfies impl::ContiguousOutputByteIt, i.e., COutByteIt is output and contiguous iterator, that iterates over values with size of 1
+     * @param[in] n integer value in network byte order
+     * @param[in,out] dst the beginning of the memory region to store n. dst must point to memory region of at least sizeof(T) size, otherwise the behaviour is
+     * undefined
+     */
+    template<std::integral T, impl::ContiguousOutputByteIt COutByteIt> constexpr void ntoh(T n, COutByteIt dst) noexcept {
         using value_type = std::remove_cvref_t<T>;
 
-        auto v = ntoh(value);
+        auto v = ntoh(n);
 
 #ifdef __cpp_lib_bit_cast
         // Thanks to constexpr bit_cast we can evaluate this expression during compilation
@@ -100,19 +159,43 @@ namespace ecpp {
 #endif
     }
 
-
-    template<std::integral T> [[nodiscard]] constexpr T hton(T value) noexcept {
-        return ntoh(value);
+    /**
+     * @brief Convert value from host byte order to network byte order
+     *
+     * @tparam T type that sarisfies std::integral, i.e., T is an integer type. The program is ill-formed if T has padding bits
+     * @param[in] n integer value in host byte order
+     * @return An integer value of type T whose object representation comprises the bytes of that of n in network byte order
+     */
+    template<std::integral T> [[nodiscard]] constexpr T hton(T n) noexcept {
+        return ntoh(n);
     }
 
 
+    /**
+     * @brief Returns value of type T by copying sizeof(T) bytes from src and converting from host byte order to network byte order
+     *
+     * @tparam T type that satisfies std::integral, i.e., T is an integer type. The program is ill-formed if T has padding bits
+     * @tparam CByteIt type that satisfies impl::ContiguousByteIt, i.e., CByteIt is contiguous iterator, that iterates over values with size of 1
+     * @param[in] src the beginning of the memory region in host byte order, which should be converted to T. src must point to memory region of at least sizeof(T) size, otherwise the behaviour is
+     * undefined
+     * @return An integer value of type T whose object representation comprises the bytes of src converted to network byte order
+     */
     template<std::integral T, std::contiguous_iterator CInputIt> [[nodiscard]] constexpr T hton(CInputIt src) noexcept {
         return ntoh<T, CInputIt>(src);
     }
 
 
-    template<std::integral T, impl::ContiguousOutputByteIt COutByteIt> constexpr void hton(T value, COutByteIt dst) noexcept {
-        return ntoh(value, dst);
+    /**
+     * @brief Converts n from host byte order to network byte order, and copies it into dst
+     *
+     * @tparam T type that satisfies std::integral, i.e., T is an integer type. The program is ill-formed if T has padding bits
+     * @tparam COutByteIt type that satisfies impl::CContiguousOutputByteIt, i.e., COutByteIt is output and contiguous iterator, that iterates over values with size of 1
+     * @param[in] n integer value in host byte order
+     * @param[in,out] dst the beginning of the memory region to store n. dst must point to memory region of at least sizeof(T) size, otherwise the behaviour is
+     * undefined
+     */
+    template<std::integral T, impl::ContiguousOutputByteIt COutByteIt> constexpr void hton(T n, COutByteIt dst) noexcept {
+        return ntoh(n, dst);
     }
 
 
