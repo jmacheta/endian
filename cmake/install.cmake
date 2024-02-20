d7178b309eb74ec3f53f6d59fd5c956e1df02398
Jan Macheta <jan.macheta@embe.tech>
2024-02-20 13:43:12 +0100

    add install target

---
+++ cmake/install.cmake
@@ -0,0 +1,20 @@
+include(CMakePackageConfigHelpers)
+
+set(CMAKES_EXPORT_DIR cmake/endian)
+
+write_basic_package_version_file(endian-version.cmake COMPATIBILITY SameMajorVersion)
+
+configure_package_config_file(
+  ${CMAKE_CURRENT_LIST_DIR}/endian-config.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/endian-config.cmake
+  INSTALL_DESTINATION ${CMAKES_EXPORT_DIR}
+)
+
+include(GNUInstallDirs)
+install(TARGETS endian EXPORT endian_targets)
+
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/ecpp DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/endian-config.cmake
+              ${CMAKE_CURRENT_BINARY_DIR}/endian-version.cmake DESTINATION ${CMAKES_EXPORT_DIR}
+)
+
+install(EXPORT endian_targets NAMESPACE ecpp:: DESTINATION ${CMAKES_EXPORT_DIR})
