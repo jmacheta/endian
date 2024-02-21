99505f6822aa36f171ea05630b723a6b451f0797
Jan Macheta <jan.macheta@embe.tech>
2024-02-21 14:19:16 +0100

    restore old install dir

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,6 +1,6 @@
 include(CMakePackageConfigHelpers)
 
-set(CMAKES_EXPORT_DIR cmake)
+set(CMAKES_EXPORT_DIR cmake/endian)
 
 write_basic_package_version_file(endian-version.cmake COMPATIBILITY SameMajorVersion)
 
